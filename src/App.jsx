0bb45c9460935ccdafe69fc1f10cf6ead2d5f219
Hcode30 <hcode023@gmail.com>
2023-05-15 13:30:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Form from './form';
+const App = () => {
+  return (
+    <>
+      <Form />
+    </>
+  );
+};
+
+export default App;
---
+++ src/form.jsx
@@ -0,0 +1,88 @@
+import './formStyle.css';
+
+import { useForm } from 'react-hook-form';
+import { object, string, number, ref } from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const Form = () => {
+  const schema = object().shape({
+    fullName: string().required(),
+    email: string().email().required(),
+    age: number().positive().integer().min(5).max(120).required(),
+    password: string().min(8).max(24).required(),
+    password2: string()
+      .oneOf([ref('password'), null])
+      .required(),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+  const onSubmit = (data) => {
+    delete data.password2;
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="fullName">
+        Full Name
+        <input
+          type="text"
+          id="fullName"
+          placeholder="Full Name ..."
+          {...register('fullName')}
+        />
+        {errors.fullName && <p>Full name is a required field</p>}
+      </label>
+
+      <label htmlFor="email">
+        Email
+        <input
+          type="text"
+          id="email"
+          placeholder="Email ..."
+          {...register('email')}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+      </label>
+
+      <label htmlFor="age">
+        Age
+        <input
+          type="number"
+          id="age"
+          placeholder="Age ..."
+          {...register('age')}
+        />
+        {errors.age && <p>Age must be in range of 5 to 120 years</p>}
+      </label>
+
+      <label htmlFor="password">
+        Password
+        <input
+          type="password"
+          id="password"
+          placeholder="Password ..."
+          {...register('password')}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+      </label>
+
+      <label htmlFor="password2">
+        Confirm Password
+        <input
+          type="password"
+          id="password2"
+          placeholder="Confirm Password ..."
+          {...register('password2')}
+        />
+        {errors.password2 && <p>The passwords don&apos;t match</p>}
+      </label>
+
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default Form;
